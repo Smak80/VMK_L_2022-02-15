33c4fe73c1001d3283186a55033b4da29f52435e
Sergey Makletsov <smaklets@kpfu.ru>
2022-02-15 09:54:58 +0300

    Добавьте файлы проекта.

---
+++ VMK_L_2022-02-15/MyClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMK_L_2022_02_15;
+
+public class MyClass
+{
+    private int x;
+    private string str;
+
+    public MyClass()
+    {
+        //x = 0;
+        str = "";
+    }
+
+    public MyClass(int a, string s)
+    {
+        (x, str) = (a, s);
+    }
+
+    public MyClass(string str)
+    {
+        this.str = str;
+    }
+
+    public override string ToString()
+    {
+        return $"x={x}, str={str}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+        if (!(obj is MyClass)) return false;
+        return (x == ((MyClass)obj).x && str.Equals(((MyClass)obj).str));
+    }
+}
---
+++ VMK_L_2022-02-15/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using VMK_L_2022_02_15;
+
+var a = new MyClass();
+var b = new MyClass();
+Console.WriteLine(a);
+Console.WriteLine(a.Equals(b));
+var c = a;
+Console.WriteLine(c.Equals(a));
